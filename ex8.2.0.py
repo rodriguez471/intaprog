1f46bd96d4effbaf11fd85b32a89e04b9f23de82
rodriguez471 <rafaelrodrigues18022010@gmail.com>
2025-09-18 10:35:43 -0300

    Add files via upload

---
+++ ex8.2.0.py
@@ -0,0 +1,59 @@
+print("************* OPERAÇÕES MATEMÁTICA ************")
+print("ESCOLHA UMA DAS OPÇÕES ABAIXO. PARA ENCERRAR DIGITE SAIR!!!")
+print("1 - SOMA")
+print("2 - SUBTRAÇÃO")
+print("3 - DIVISÃO")
+print("4 - MULTIPLICAÇÃO")
+print("5 - PAR OU ÍMPAR")
+print("6 - PRIMO")
+print("7 - FATORIAL")
+opção=input("digite a opção desejada ou <SAIR> para encerrar:")
+opçãoMaiusc=opção.upper()
+while opçãoMaiusc!="SAIR":
+    if opção=="1":
+        num1=int(input("digite o primeiro valor: "))
+        num2=int(input("digite o segundo valo: "))      
+        print("a soma dos valores é:",num1+num2)
+    if opção=="2":
+     num1=int(input("digite o primeiro valor: "))
+    num2=int(input("digite o segundo valor: "))
+    print("a subtração dos valorees é:",num1-num2)
+    if opção=="3":
+        num1=int(input("digite o primeiro valor: "))
+    num2=int(input("digite o segundo valor: "))
+    print("a multiplicação dos valorees é:",num1*num2)
+    if opção=="4":
+     num1=int(input("digite o primeiro valor: "))
+    num2=int(input("digite o segundo valor: "))
+    print("a divisão dos valorees é:",num1/num2)
+input("pressione ENTER para voltar ao menu!")
+#os.system("cls")
+
+
+print("************* OPERAÇÕES MATEMÁTICA ************")
+print("ESCOLHA UMA DAS OPÇÕES ABAIXO. PARA ENCERRAR DIGITE SAIR!!!")
+print("1 - SOMA")
+print("2 - SUBTRAÇÃO")
+print("3 - DIVISÃO")
+print("4 - MULTIPLICAÇÃO")
+print("5 - PAR OU ÍMPAR")
+print("6 - PRIMO")
+print("7 - FATORIAL")
+
+if opção== "5":
+    num1=int(input("digite um número para saber se ele é par ou ímpar:"))
+    if num1%2==0:
+        print("o número é par!")
+    else:
+        print("o número é ímpar!")
+input("pressione ENTER para voltar ao menu!")
+ 
+print("ESCOLHA UMA DAS OPÇÕES ABAIXO. PARA ENCERRAR DIGITE SAIR!!!")
+print("1 - SOMA")
+print("2 - SUBTRAÇÃO")
+print("3 - DIVISÃO")
+print("4 - MULTIPLICAÇÃO")
+print("5 - PAR OU ÍMPAR")
+print("6 - PRIMO")
+print("7 - FATORIAL")
+        
